3ad9899ce25370b1ad65bd9c653d4317f80d0b39
Alex Kobyakovski <74356061+Hrafnawig@users.noreply.github.com>
2021-03-07 16:23:30 +0200

    Delete study/easylvl/src directory

--- study/easylvl/src/Components/Buttons.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-export default function Buttons(props) {
-    return (
-        <div>
-            <button onClick={() => props.setCount(props.count + 1)}>Добавить</button>
-        </div>
-    )
-}
